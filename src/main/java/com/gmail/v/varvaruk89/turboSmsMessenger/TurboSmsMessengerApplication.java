db5854d453d741f1ce521bd1cc160f9f1a065fd2
VADUM vARVARUK <v.varvaruk89@gmail.com>
2018-02-16 09:57:44 +0200

    start

---
+++ src/main/java/com/gmail/v/varvaruk89/turboSmsMessenger/TurboSmsMessengerApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.v.varvaruk89.turboSmsMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboSmsMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboSmsMessengerApplication.class, args);
+	}
+}
